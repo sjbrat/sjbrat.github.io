3549ef98a9cc822a39f478603a344cc9327fbcf2
SJ Brat <justanobser@gmail.com>
2019-05-24 18:16:38 -0400

    back button/moving letter/

--- js/index.js
+++ js/index.js
@@ -14,6 +14,33 @@ $(function () {
     $(this).toggleClass('flipped')
   }
 
+  // Wrap every letter in a span
+  $('.thankyou').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
 
+  anime.timeline({loop: true})
+    .add({
+      targets: '.thankyou .letter',
+      translateX: [40,0],
+      translateZ: 0,
+      opacity: [0,1],
+      easing: "easeOutExpo",
+      duration: 1200,
+      delay: function(el, i) {
+        return 500 + 30 * i;
+      }
+    }).add({
+      targets: '.thankyou .letter',
+      translateX: [0,-30],
+      opacity: [1,0],
+      easing: "easeInExpo",
+      duration: 1100,
+      delay: function(el, i) {
+        return 100 + 30 * i;
+      }
+    });
+
+    $('textarea').autoResize();
 
 })
