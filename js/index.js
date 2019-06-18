7a45cf95962c72d3a1729c6d4d657e74c01882f7
Kareem Grant <kareem@getuserwise.com>
2019-06-18 12:58:06 -0400

    Added jQuery Flip library to fix responsive flip issue, updated card style

--- js/index.js
+++ js/index.js
@@ -7,42 +7,55 @@ $(function () {
     fitWidth: true
   })
 
-  $('.card').click(flip)
+  // $('.card').click(flip)
+  //
+  // function flip () {
+  //   $('.card').removeClass('flipped')
+  //   $(this).toggleClass('flipped')
+  // }
 
-  function flip () {
-    $('.card').removeClass('flipped')
-    $(this).toggleClass('flipped')
+  $('.card').flip({
+    trigger: 'click'
+  })
+
+  // $('.card').click(handleFlip)
+
+  function handleFlip () {
+    // 1. unflip all other cards that are flipped
+    // $('.card').flip(false)
+
+    // 2. flip the selected card
+    $(this).flip({
+      trigger: 'manual'
+    })
   }
 
   // Wrap every letter in a span
-  $('.thankyou').each(function(){
-    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-  });
+  $('.thankyou').each(function () {
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"))
+  })
 
   anime.timeline({loop: true})
     .add({
       targets: '.thankyou .letter',
-      translateX: [40,0],
+      translateX: [40, 0],
       translateZ: 0,
-      opacity: [0,1],
-      easing: "easeOutExpo",
+      opacity: [0, 1],
+      easing: 'easeOutExpo',
       duration: 1200,
-      delay: function(el, i) {
-        return 500 + 30 * i;
+      delay: function (el, i) {
+        return 500 + 30 * i
       }
     }).add({
       targets: '.thankyou .letter',
-      translateX: [0,-30],
-      opacity: [1,0],
-      easing: "easeInExpo",
+      translateX: [0, -30],
+      opacity: [1, 0],
+      easing: 'easeInExpo',
       duration: 1100,
-      delay: function(el, i) {
-        return 100 + 30 * i;
+      delay: function (el, i) {
+        return 100 + 30 * i
       }
-    });
+    })
 
 // autosize
-
-
-
 })
